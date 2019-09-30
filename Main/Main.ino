6212321899048b65cad4b46adbb37f2ab96a739b
Hesli Azevedo <sis-hesliazevedo@uniguacu.edu.br>
2019-09-30 20:31:06 -0300

    TESTES feitos

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,25 +1,41 @@
+#include <Ultrasonic.h>
+
 #include <AFMotor.h>
-#include <Ultrassonic.h>
 
 
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
+
+AF_DCMotor motor1(3);
+AF_DCMotor motor2(4);
 
 /*Definir posição e portas utilizadas*/
-//Ultrassonic Distancia();
+Ultrasonic Distancia(53, 52);
 
 
 void setup() {
     motor1.setSpeed(255);
     motor2.setSpeed(255);
-
+    Serial.begin(9600);
 }
 
 void loop() {
   //Lógica
+  float cm;
+  long tempo = Distancia.timing();
+  
+  cm = Distancia.convert(tempo, Ultrasonic::CM);
 
+  Serial.println(cm);
 
+  if(cm <= 6){
+      motor1.run(FORWARD);
+      motor2.run(FORWARD);
+    }else{
+        motor1.run(RELEASE);
+        motor2.run(RELEASE);
+      }
+
+/* Código da linha preta*/
 
   
-  çç
+  
 }

adc2321bbcfe252216a683af965516941197ca9c
Hesli Azevedo <sis-hesliazevedo@uniguacu.edu.br>
2019-10-07 19:36:49 -0300

    Iniciado correção de código e definição geral das portas do arduino

--- Main/Main.ino
+++ Main/Main.ino
@@ -9,31 +9,40 @@ AF_DCMotor motor2(4);
 
 /*Definir posição e portas utilizadas*/
 Ultrasonic Distancia(53, 52);
+int SensorLinha = 51;
 
 
 void setup() {
     motor1.setSpeed(255);
     motor2.setSpeed(255);
     Serial.begin(9600);
+
+    //Definição dos sensores
+    pinMode(SensorLinha, INPUT);
 }
 
 void loop() {
   //Lógica
   float cm;
   long tempo = Distancia.timing();
-  
-  cm = Distancia.convert(tempo, Ultrasonic::CM);
 
-  Serial.println(cm);
 
-  if(cm <= 6){
+  //Código para localizar o oponente
+
+  while(cm >= 15){
+      motor1.run(FORWARD);
+      motor2.run(BACKWARD);
+    }
+
+  //Caso localize o oponente irá acelerar os motores
+  if(cm <= 15){
       motor1.run(FORWARD);
       motor2.run(FORWARD);
-    }else{
-        motor1.run(RELEASE);
-        motor2.run(RELEASE);
-      }
-
+      if(digitalRead(SensorLinha) == HIGH){
+            motor1.run(RELEASE);
+            motor2.run(RELEASE);
+        }
+    }
 /* Código da linha preta*/
 
   
